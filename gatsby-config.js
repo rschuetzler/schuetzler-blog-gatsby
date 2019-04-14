dcb36ad6d86101bbff6b844e3e003b34ba9eae44
Ryan Schuetzler <ryan@schuetzler.net>
2019-04-14 15:48:28 -0500

    Update site title and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `Gatsby Starter Blog`,
-				short_name: `GatsbyJS`,
+				name: `Ryan Schuetzler`,
+				short_name: `Schuetzler.net`,
 				start_url: `/`,
 				background_color: `#ffffff`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `content/assets/gatsby-icon.png`
+				icon: `content/assets/icon.png`
 			}
 		},
 		`gatsby-plugin-offline`,
