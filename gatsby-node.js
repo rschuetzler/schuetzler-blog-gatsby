a69efe0d391e68e48f36762f4d47c5b2ef0fdf43
Ryan Schuetzler <ryan@schuetzler.net>
2019-04-17 16:13:27 -0500

    Add basic functioning archive page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
-const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
-const createPaginatedPages = require('gatsby-paginate');
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const createPaginatedPages = require(`gatsby-paginate`)
 
 exports.createPages = ({ graphql, actions }) => {
-	const { createPage } = actions;
+  const { createPage } = actions
 
-	const blogPost = path.resolve(`./src/templates/blog-post.js`);
-	return graphql(
-		`
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  return graphql(
+    `
       {
         allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
@@ -18,59 +18,64 @@ exports.createPages = ({ graphql, actions }) => {
               fields {
                 slug
               }
+              excerpt(pruneLength: 160)
+              html
               frontmatter {
                 title
+                date(formatString: "MMMM DD, YYYY")
+                description
               }
             }
           }
         }
       }
     `
-	).then((result) => {
-		if (result.errors) {
-			throw result.errors;
-		}
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
 
-		// Create blog posts pages.
-		const posts = result.data.allMarkdownRemark.edges;
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
 
-		createPaginatedPages({
-			edges: posts,
-			createPage: createPage,
-			pageTemplate: 'src/templates/archive.js',
-			pageLength: 10, // This is optional and defaults to 10 if not used
-			pathPrefix: 'archive', // This is optional and defaults to an empty string if not used
-			buildPath: (index, pathPrefix) => index > 1 ? `${pathPrefix}/${index}` : `/${pathPrefix}`
-		});
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
 
-		posts.forEach((post, index) => {
-			const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-			const next = index === 0 ? null : posts[index - 1].node;
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
 
-			createPage({
-				path: post.node.fields.slug,
-				component: blogPost,
-				context: {
-					slug: post.node.fields.slug,
-					previous,
-					next
-				}
-			});
-		});
+    createPaginatedPages({
+      edges: posts,
+      createPage: createPage,
+      pageTemplate: "src/templates/archive.js",
+      pageLength: 10, // This is optional and defaults to 10 if not used
+      pathPrefix: "archive", // This is optional and defaults to an empty string if not used
+      buildPath: (index, pathPrefix) =>
+        index > 1 ? `${pathPrefix}/${index}` : `/${pathPrefix}`,
+    })
 
-		return null;
-	});
-};
+    return null
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-	const { createNodeField } = actions;
+  const { createNodeField } = actions
 
-	if (node.internal.type === `MarkdownRemark`) {
-		const value = createFilePath({ node, getNode });
-		createNodeField({
-			name: `slug`,
-			node,
-			value
-		});
-	}
-};
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
